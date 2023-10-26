9abdcd820f24c74b3c5684bfe9c93fab5a6b48ee
longyangzz <longyangzz@outlook.com>
2023-10-26 16:02:09 +0800

    fix: 初始定位修改

---
+++ GisMap/LayerManager.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2.GisMap
+{
+    internal class LayerManager
+    {
+        public void AddLayer()
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Filter = @"Shapefiles (*.shp)|*.shp";
+            if (ofd.ShowDialog() == true)
+            {
+                
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Dialog;
+using Mapsui;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,6 +21,20 @@ namespace WpfApp2
         public MainWindow()
         {
             InitializeComponent();
+
+            //构建一个底图引擎
+            var mapControl = new Mapsui.UI.Wpf.MapControl();
+
+            mapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+            //初始化 定位中国经纬度范围
+            MRect mRect = new MRect(73.44696044921875, 18.15999984741211, 135.08544921875, 53.560001373291016);
+            //mapControl.Map.Navigator.ZoomToBox(mRect);
+
+            //map.CRS = "EPSG:3857";
+            mapControl.Map.Home = n => n.CenterOnAndZoomTo(new MPoint(12412846.80014016, 3664472.687460804), n.Resolutions[4]);
+
+            //添加到 GridContent中
+            GridMapContent.Children.Add(mapControl);
         }
 
         private void ButtonPopUpLogout_Click(object sender, RoutedEventArgs e)
