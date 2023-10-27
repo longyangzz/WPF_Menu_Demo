febf97b5c9be82135c054a1dec13dc0175e420c3
longyangzz <longyangzz@outlook.com>
2023-10-27 10:24:54 +0800

    fix: 添加测试 图片ok

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,18 +23,18 @@ namespace WpfApp2
             InitializeComponent();
 
             //构建一个底图引擎
-            var mapControl = new Mapsui.UI.Wpf.MapControl();
+            //var mapControl = new Mapsui.UI.Wpf.MapControl();
 
-            mapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+            //mapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
             //初始化 定位中国经纬度范围
-            MRect mRect = new MRect(73.44696044921875, 18.15999984741211, 135.08544921875, 53.560001373291016);
+            //MRect mRect = new MRect(73.44696044921875, 18.15999984741211, 135.08544921875, 53.560001373291016);
             //mapControl.Map.Navigator.ZoomToBox(mRect);
 
             //map.CRS = "EPSG:3857";
-            mapControl.Map.Home = n => n.CenterOnAndZoomTo(new MPoint(12412846.80014016, 3664472.687460804), n.Resolutions[4]);
+            //mapControl.Map.Home = n => n.CenterOnAndZoomTo(new MPoint(12412846.80014016, 3664472.687460804), n.Resolutions[4]);
 
             //添加到 GridContent中
-            GridMapContent.Children.Add(mapControl);
+            //GridMapContent.Children.Add(mapControl);
         }
 
         private void ButtonPopUpLogout_Click(object sender, RoutedEventArgs e)
